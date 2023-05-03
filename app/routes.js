7241894d203c4cd67aff10c8a17b743d01c01aed
jonny-monk <92162962+jonny-monk@users.noreply.github.com>
2023-05-03 11:53:31 +0100

    add help with applying route

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,11 @@
+const url = require('url');
+
+
+
+
+
+
+
 //
 // For guidance on how to create routes see:
 // https://prototype-kit.service.gov.uk/docs/create-routes
@@ -13,3 +21,62 @@ router.get('/', function (req, res) {
 
 
 // Add your routes here
+
+router.get('/l3-help-with-applying', function (req, res) {
+  var referer=req.headers.referer
+  const refererUrl = new URL(referer);
+  var page=refererUrl.pathname
+  var file=page.substring(page.lastIndexOf('/')+1)
+  console.log(referer)
+  console.log(page)
+  console.log(file)
+  var pagename=""
+  if(file === "l2-adults") {
+  pagename="Adults"
+  }
+  else if(file === "l2-young-people"){
+  pagename="Young People" 
+  }
+  req.session['referingroute']=pagename
+  req.session['referingUrl']=page
+  res.render('l3-help-with-applying', {pagename,page})
+})
+
+
+
+router.get('/l4-cv', function (req, res) {
+  var pagename=""
+  if(req.session['referingroute'] === "Adults") {
+  pagename="Adults"
+  }
+  else if(req.session['referingroute'] === "Young People"){
+  pagename="Young People" 
+  }
+  var page=req.session['referingUrl']
+  res.render('l4-cv', {pagename,page})
+})
+
+router.get('/l4-interview-tips', function (req, res) {
+  var pagename=""
+  if(req.session['referingroute'] === "Adults") {
+  pagename="Adults"
+  }
+  else if(req.session['referingroute'] === "Young People"){
+  pagename="Young People" 
+  }
+  var page=req.session['referingUrl']
+  res.render('l4-interview-tips', {pagename,page})
+})
+
+router.get('/l4-application-form', function (req, res) {
+  var pagename=""
+  if(req.session['referingroute'] === "Adults") {
+  pagename="Adults"
+  }
+  else if(req.session['referingroute'] === "Young People"){
+  pagename="Young People" 
+  }
+  var page=req.session['referingUrl']
+  res.render('l4-application-form', {pagename,page})
+})
+
